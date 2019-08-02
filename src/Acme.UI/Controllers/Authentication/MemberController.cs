24fac868305f9aea96a80c9b4b2e5e6b59ffa898
Mohammad Kamran <mohammad.kamran@ie.com.au>
2019-08-02 14:41:42 +1000

    LoginPage - The initial implementation of Login Page

---
+++ src/Acme.UI/Controllers/Authentication/MemberController.cs
@@ -0,0 +1,55 @@
+﻿using Acme.UI.Models.Authentication;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Acme.UI.Controllers.Authentication
+{
+    public class MemberController : SurfaceController
+    {
+        public ActionResult RenderLogin()
+        {
+            return PartialView("~/Views/Authentication/_Login.cshtml", new LoginModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    UrlHelper helper = new UrlHelper(HttpContext.Request.RequestContext);
+                    if (helper.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return Redirect("/login/");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username/password");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView("~/Views/Authentication/_Logout.cshtml", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ src/Acme.UI/Models/Authentication/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Acme.UI.Models.Authentication
+{
+    public class LoginModel
+    {
+        [Display(Name = "Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+    }
+}
