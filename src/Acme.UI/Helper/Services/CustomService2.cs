81bba48165f0b6947945c800d56c14b42973525f
Mohammad Kamran <mohammad.kamran@ie.com.au>
2019-08-20 13:18:41 +1000

    DI - Umbraco Composer implementation

---
+++ src/Acme.UI/Helper/Services/CustomService2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Acme.UI.Helper.Services
+{
+    public class CustomService2 : IAcmeService<string, string>
+    {
+        public string Process(string arg)
+        {
+            return $" - Processed2: {arg}";
+        }
+    }
+}
