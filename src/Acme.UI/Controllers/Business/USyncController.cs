0abdf7d7efb2ee2f67193b327136ac09837126fa
Mohammad Kamran <mohammad.kamran@ie.com.au>
2019-09-05 14:41:19 +1000

    Import Content via uSyncService

---
+++ src/Acme.UI/Controllers/Business/USyncController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web.WebApi;
+using uSync8.BackOffice;
+using Acme.UI.Helper.Extensions;
+
+namespace Acme.UI.Controllers.Business
+{
+    
+    public class USyncController : UmbracoApiController
+    {
+        private uSyncService _uSyncService;
+        private IEnumerable<object> Environment;
+
+        public USyncController(uSyncService uSyncService)
+        {
+            _uSyncService = uSyncService;
+        }
+    
+
+        [HttpGet]
+        public string GetAutoImportContent(string path)
+        {
+            var result = _uSyncService.Import(path, true);
+
+            var res = new List<string>(GetSuccessMessages(result));
+            res.AddRange(GetFailureMessages(result));
+            var msg = string.Join(System.Environment.NewLine, res);
+
+            return "Completed Import. \n" + msg; // we need to further check action semantics in terms of import success or failure. so far the
+        }
+
+
+        private IEnumerable<string> GetSuccessMessages(IEnumerable<uSyncAction> actions)
+        {
+            if (!actions.HasValues())
+            {
+                return null;
+            }
+
+            return actions.Where(act => act.Success).Select(x => "[Successfully Processed: " +  x.FileName + "]") .ToList();
+        }
+
+        private IEnumerable<string> GetFailureMessages(IEnumerable<uSyncAction> actions)
+        {
+            if (!actions.HasValues())
+            {
+                return null;
+            }
+
+            return actions.Where(act => !act.Success).Select(x => "[Failed: " + x.FileName + " - " + x.Message + "]").ToList();
+        }
+    }
+}
---
+++ src/Acme.UI/Helper/Extensions/CollectionExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Models;
+
+namespace Acme.UI.Helper.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static bool HasValues<T>(this IEnumerable<T> obj)
+        {
+            return obj != null && obj.Any();
+        }
+
+        public static void AddIfNotNull<T>(this ICollection<T> obj, T item)
+        {
+            if (item != null)
+            {
+                obj.Add(item);
+            }
+        }
+
+
+        public static bool NotNullAndContainsKey<TKey, TValue>(this IDictionary<TKey, TValue> obj, TKey key)
+        {
+            return obj != null && obj.Keys != null && obj.Keys.Contains(key);
+        }
+
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> obj, TKey key)
+        {
+            if (obj.NotNullAndContainsKey(key))
+            {
+                return obj[key];
+            }
+
+            return default(TValue);
+        }
+    }
+}
